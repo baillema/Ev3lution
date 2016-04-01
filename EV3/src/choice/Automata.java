52a26cb39102260f3ace3ef7a4a46f63b7d264f6
baillema <mrmarthieu@gmail.com>
2016-04-01 09:46:42 +0200

    Ajout de log pour la sauvegarde de couleur

--- EV3/src/choice/Automata.java
+++ EV3/src/choice/Automata.java
@@ -2,4 +2,15 @@ package choice;
 
 public class Automata {
 
+	enum state {
+        Wait_button,
+        Search,
+        SearchD,
+        SearchG,
+        Detected,
+        Catch,
+        Return
+    };
+    
+    
 }
